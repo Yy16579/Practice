6e5b5dcafac6bd876e4c878d7da6e26372d3c394
Yy16579 <1657947999@qq.com>
2026-03-29 16:47:27 +0800

    022_leetcode_getIntersectionNode

---
+++ 022_leetcode_getIntersectionNode.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* A = headA;
+        ListNode* B = headB;
+        while(A != B){
+            A = (A == nullptr ? headB : A->next);
+            B = (B == nullptr ? headA : B->next);
+        }
+        return A;
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
