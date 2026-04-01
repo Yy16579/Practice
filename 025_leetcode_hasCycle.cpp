2670e05370277e5598023008c3ec95219b8f3d5f
Yy16579 <1657947999@qq.com>
2026-04-01 15:26:13 +0800

    025_leetcode_hasCycle

---
+++ 025_leetcode_hasCycle.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* f = head;
+        ListNode* s = head;
+        while(f != nullptr && f->next != nullptr){
+            s = s->next;
+            f = f->next->next;
+            if(s == f){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
